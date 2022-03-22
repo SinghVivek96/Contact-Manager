418e0735e04e8ef8842ff8a7066afb1d0f74cbb7
SinghVivek96 <itsvivek96@gmail.com>
2022-03-23 00:16:41 +0530

    Thymeleaf added

--- Contact-Manager/src/main/java/com/scm/contact_manager/controller/HomeController.java
+++ Contact-Manager/src/main/java/com/scm/contact_manager/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +17,30 @@ public class HomeController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	
+	@GetMapping("/home")
+	public String base(Model model) {
+		model.addAttribute("title","Home - Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Contact Manager");
+		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title","Register - Contact Manager");
+		return "signup";
+	}
+	
+//	@GetMapping("/base")
+//	public String home() {
+//		return "base";
+//	}
 	
 	@GetMapping("/test")
 	@ResponseBody
